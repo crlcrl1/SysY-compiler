d1895f6d9398fb2c7258e2f12ad5dd1773e18791
crlcrl1 <crl18039102576@126.com>
2024-10-03 18:59:16 +0800

    Ast structs and enums.

---
+++ src/ast.rs
@@ -0,0 +1,258 @@
+#[derive(Debug, PartialEq, Clone)]
+pub struct CompUnit {
+    items: Vec<GlobalItem>,
+}
+
+#[derive(Debug, PartialEq, Clone)]
+pub enum GlobalItem {
+    Decl(Decl),
+    FuncDef(FuncDef),
+}
+
+/// Represent const declaration or non-const variable declaration.
+///
+/// One declaration statement may contains more than one declaration.
+#[derive(Debug, PartialEq, Clone)]
+pub enum Decl {
+    ConstDecl(Vec<ConstDef>),
+    VarDecl(Vec<VarDef>),
+}
+
+#[derive(Debug, PartialEq, Clone)]
+pub enum ConstDef {
+    NormalConstDef(NormalConstDef),
+    ArrayConstDef(ArrayConstDef),
+}
+
+#[derive(Debug, PartialEq, Clone)]
+pub struct NormalConstDef {
+    name: String,
+    value: ConstExpr,
+}
+
+#[derive(Debug, PartialEq, Clone)]
+pub struct ArrayConstDef {
+    name: String,
+    shape: Vec<ConstExpr>,
+    values: ConstArray,
+}
+
+#[derive(Debug, PartialEq, Clone)]
+pub enum ConstArray {
+    Val(Vec<i32>),
+    Array(Vec<ConstArray>),
+}
+
+#[derive(Debug, PartialEq, Clone)]
+pub enum VarDef {
+    NormalVarDef(NormalVarDef),
+    ArrayVarDef(ArrayVarDef),
+}
+
+#[derive(Debug, PartialEq, Clone)]
+pub struct NormalVarDef {
+    name: String,
+    value: Option<Expr>,
+}
+
+#[derive(Debug, PartialEq, Clone)]
+pub struct ArrayVarDef {
+    name: String,
+    shape: Vec<ConstDef>,
+    values: Option<ExprArray>,
+}
+
+#[derive(Debug, PartialEq, Clone)]
+pub enum ExprArray {
+    Val(Expr),
+    Array(Vec<ExprArray>),
+}
+
+#[derive(Debug, PartialEq, Clone)]
+pub struct Expr(Box<AddExpr>);
+
+#[derive(Debug, PartialEq, Clone)]
+pub struct FuncDef {
+    name: String,
+    params: Vec<FuncFParam>,
+    ret_type: FuncType,
+    body: Block,
+}
+
+/// Represent function parameter in declaration.
+#[derive(Debug, PartialEq, Clone)]
+pub enum FuncFParam {
+    NormalFParam(NormalFParam),
+    ArrayFParam(ArrayFParam),
+}
+
+#[derive(Debug, PartialEq, Clone)]
+pub struct NormalFParam {
+    name: String,
+}
+
+#[derive(Debug, PartialEq, Clone)]
+pub struct ArrayFParam {
+    name: String,
+    /// Whether the array has a placeholder. For example, `int a[]` has a placeholder.
+    placeholder: bool,
+    shape: Vec<i32>,
+}
+
+#[derive(Debug, PartialEq, Clone)]
+pub enum FuncType {
+    Void,
+    Int,
+}
+
+#[derive(Debug, PartialEq, Clone)]
+pub struct Block {
+    items: Vec<BlockItem>,
+}
+
+#[derive(Debug, PartialEq, Clone)]
+pub enum BlockItem {
+    Stmt(Stmt),
+    Decl(Decl),
+}
+
+#[derive(Debug, PartialEq, Clone)]
+pub enum Stmt {
+    Assign(Assign),
+    Expr(Expr),
+    Block(Block),
+    If(If),
+    While(While),
+    Return(Return),
+    Break,
+    Continue,
+}
+
+#[derive(Debug, PartialEq, Clone)]
+pub struct Assign {
+    target: LVal,
+    value: Expr,
+}
+
+#[derive(Debug, PartialEq, Clone)]
+pub enum LVal {
+    Var(String),
+    ArrayElem(ArrayElem),
+}
+
+#[derive(Debug, PartialEq, Clone)]
+pub struct ArrayElem {
+    name: String,
+    indices: Vec<Expr>,
+}
+
+#[derive(Debug, PartialEq, Clone)]
+pub struct If {
+    cond: LOrExpr,
+    then_stmt: Box<Stmt>,
+    else_stmt: Option<Box<Stmt>>,
+}
+
+#[derive(Debug, PartialEq, Clone)]
+pub struct While {
+    cond: LOrExpr,
+    body: Box<Stmt>,
+}
+
+#[derive(Debug, PartialEq, Clone)]
+pub struct Return {
+    return_value: Option<Expr>,
+}
+
+#[derive(Debug, PartialEq, Clone)]
+pub enum LOrExpr {
+    LAndExpr(LAndExpr),
+    Or(Box<LOrExpr>, Box<LAndExpr>),
+}
+
+#[derive(Debug, PartialEq, Clone)]
+pub enum LAndExpr {
+    EqExpr(EqExpr),
+    And(Box<LAndExpr>, Box<EqExpr>),
+}
+
+#[derive(Debug, PartialEq, Clone)]
+pub enum EqOp {
+    Eq,
+    Ne,
+}
+
+#[derive(Debug, PartialEq, Clone)]
+pub enum EqExpr {
+    RelExpr(RelExpr),
+    Eq(Box<EqExpr>, EqOp, Box<RelExpr>),
+}
+
+#[derive(Debug, PartialEq, Clone)]
+pub enum RelOp {
+    Lt,
+    Gt,
+    Le,
+    Ge,
+}
+
+#[derive(Debug, PartialEq, Clone)]
+pub enum RelExpr {
+    AddExpr(AddExpr),
+    Rel(Box<RelExpr>, RelOp, Box<AddExpr>),
+}
+
+#[derive(Debug, PartialEq, Clone)]
+pub enum AddOp {
+    Add,
+    Sub,
+}
+
+#[derive(Debug, PartialEq, Clone)]
+pub enum AddExpr {
+    MulExpr(MulExpr),
+    Add(Box<AddExpr>, AddOp, Box<MulExpr>),
+}
+
+#[derive(Debug, PartialEq, Clone)]
+pub enum MulOp {
+    Mul,
+    Div,
+    Mod,
+}
+
+#[derive(Debug, PartialEq, Clone)]
+pub enum MulExpr {
+    UnaryExpr(UnaryExpr),
+    Mul(Box<MulExpr>, MulOp, Box<UnaryExpr>),
+}
+
+#[derive(Debug, PartialEq, Clone)]
+pub enum UnaryOp {
+    Pos,
+    Neg,
+    Not,
+}
+
+#[derive(Debug, PartialEq, Clone)]
+pub enum UnaryExpr {
+    PrimaryExpr(PrimaryExpr),
+    FuncCall(FuncCall),
+    Unary(UnaryOp, Box<UnaryExpr>),
+}
+
+#[derive(Debug, PartialEq, Clone)]
+pub enum PrimaryExpr {
+    Expr(Expr),
+    LVal(LVal),
+    Number(i32),
+}
+
+#[derive(Debug, PartialEq, Clone)]
+pub struct FuncCall {
+    name: String,
+    args: Vec<Expr>,
+}
+
+#[derive(Debug, PartialEq, Clone)]
+pub struct ConstExpr(Box<AddExpr>);
