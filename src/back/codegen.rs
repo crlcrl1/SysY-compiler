fb45cf3d48afaafa21306863712bbedcd3d88acd
crl <crl18039102576@126.com>
2024-10-19 18:38:11 +0800

    temp commit

--- src/back/codegen.rs
+++ src/back/codegen.rs
@@ -220,15 +220,16 @@ impl ToAsm for Jump {
     type Output = Vec<Box<dyn Inst>>;
 
     fn to_asm(&self, ctx: &mut Context, program: &Program) -> Result<Self::Output, AsmError> {
-        let func = ctx.func.ok_or(AsmError::UnknownFunction)?;
-        let func_data = program.func(func);
-        let target = func_data.dfg().bb(self.target());
-        let target_name = target
-            .name()
-            .clone()
-            .map(|name| label_name(&func_data.name(), &name))
-            .unwrap_or(ctx.name_generator.generate_label_name());
-        Ok(vec![Box::new(Jmp { label: target_name })])
+        Ok(vec![])
+        // let func = ctx.func.ok_or(AsmError::UnknownFunction)?;
+        // let func_data = program.func(func);
+        // let target = func_data.dfg().bb(self.target());
+        // let target_name = target
+        //     .name()
+        //     .clone()
+        //     .map(|name| label_name(&func_data.name(), &name))
+        //     .unwrap_or(ctx.name_generator.generate_label_name());
+        // Ok(vec![Box::new(Jmp { label: target_name })])
     }
 }
 
